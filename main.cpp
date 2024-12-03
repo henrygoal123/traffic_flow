43656261a6cb823f73be80011eb34e5abd11ff9f
saic <328922675@qq.com>
2024-12-03 18:09:35 +0800

    v1release

--- main.cpp
+++ main.cpp
@@ -7,10 +7,12 @@
 #include "traffic_ele.h"
 
 bool test(){
-    T_Sim::Traffic_Ele tra_ele();
+    T_Sim::Traffic_Ele tra_ele;
     tra_ele.Add_Actor(std::make_shared<T_Sim::Actor_Car>("car1"));
     tra_ele.Add_Ego(std::make_shared<T_Sim::Ego_Car>("ego1"));
-    tra.Step();
+    for (int i = 0; i < 3; i++){
+        tra_ele.Step();   
+    }
 }
 int main(){
     if (!test()){
